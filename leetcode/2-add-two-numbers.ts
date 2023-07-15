89593fdfe9cb3a6c13b219a53b34733fe2500a2a
Hasan <dev.hahuaz@gmail.com>
2023-07-15 10:36:39 +0300

    optimize

--- leetcode/2-add-two-numbers.ts
+++ leetcode/2-add-two-numbers.ts
@@ -8,17 +8,16 @@ class ListNode {
 }
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
-  let prevNode = new ListNode(0);
-  const headNode = prevNode;
+  const headNode = new ListNode(0);
+  let prevNode = headNode;
 
   let carry = 0;
   while (l1 || l2 || carry) {
     const sum = (l1?.val || 0) + (l2?.val || 0) + carry;
     sum > 9 ? (carry = 1) : (carry = 0);
     const digit = sum % 10;
-    const currNode = new ListNode(digit);
-    prevNode.next = currNode;
-    prevNode = currNode;
+    prevNode.next = new ListNode(digit);
+    prevNode = prevNode.next;
 
     l1 = l1?.next || null;
     l2 = l2?.next || null;
