cd45716d77e7f6411d380cd1b30bccfad0006701
Hasan <dev.hahuaz@gmail.com>
2023-08-07 18:05:49 +0300

    updatge select

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -32,13 +32,11 @@ export default function CurrencySelect(
   return (
     <div className="bg-white p-32">
       <div
-        className={` relative m-4 w-[110px] justify-center border-b-2 text-gray-600  ${
-          isSelectFocused ? "border-blue-500" : "border-gray-400"
-        }`}
+        className={`m-4 flex w-[25ch] flex-col justify-center  text-gray-600`}
       >
         <label
           htmlFor="currency-select"
-          className={`pointer-events-none absolute left-0 top-0 pt-2 ${
+          className={`pointer-events-none text-sm ${
             isSelectFocused && "  text-blue-500 "
           }`}
         >
@@ -51,7 +49,13 @@ export default function CurrencySelect(
           onBlur={() => setIsSelectFocused(false)}
           onChange={handleSelectChange}
           disabled={isDisabled}
-          className={`w-full py-2 pl-[70px]  font-semibold  text-black outline-none`}
+          style={{
+            transitionDuration: "900ms",
+            transformOrigin: "mid",
+          }}
+          className={` border-b-2 font-semibold outline-none ${
+            isSelectFocused ? "border-blue-500" : "border-gray-400"
+          }`}
         >
           {currencies.map((currency) => (
             <option key={currency.value} value={currency.value}>
@@ -59,6 +63,7 @@ export default function CurrencySelect(
             </option>
           ))}
         </select>
+        <p className="text-xs text-gray-400">Please select your currency</p>
       </div>
     </div>
   );
