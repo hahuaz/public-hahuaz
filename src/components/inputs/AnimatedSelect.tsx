1d72de871db4b71f9780671f4b17a40b3c485640
Hasan <dev.hahuaz@gmail.com>
2023-08-08 11:42:48 +0300

    add comments

--- src/components/inputs/AnimatedSelect.tsx
+++ src/components/inputs/AnimatedSelect.tsx
@@ -26,7 +26,7 @@ export default function AnimatedSelect() {
       <div className="bg-gray-600 p-2">
         <motion.nav
           initial={false}
-          animate={isOpen ? "open" : "closed"}
+          animate={isOpen ? "open" : "closed"} // set variant state
           className="relative inline-block"
         >
           <motion.button
@@ -55,12 +55,12 @@ export default function AnimatedSelect() {
                   type: "spring",
                   bounce: 0,
                   duration: 0.7,
-                  delayChildren: 0.35,
+                  delayChildren: 0.35, // make li to wait untill ul is finished halfway
                   staggerChildren: 0.05,
                 },
               },
               closed: {
-                clipPath: "inset(10% 50% 90% 50% round 10px)",
+                clipPath: "inset(10% 50% 90% 50% round 10px)", // control initial state of animation. e.g. do not show 90% from bottom
                 transition: {
                   type: "spring",
                   bounce: 0,
@@ -69,7 +69,7 @@ export default function AnimatedSelect() {
               },
             }}
             style={{ pointerEvents: isOpen ? "auto" : "none" }}
-            className="absolute z-10 mt-2 w-full bg-gray-100"
+            className="absolute z-10 mt-2 w-full bg-gray-100" // By setting the ul element's position as "absolute," it is removed from the normal document flow, allowing it to overlap other elements without affecting their placement.
           >
             <motion.li
               variants={itemVariants}
