8085138e77e4c988168b6a9c8b8123c0bfcb725d
Hasan <dev.hahuaz@gmail.com>
2023-08-08 12:14:34 +0300

    organize directory structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
-export { default as Autocomplete } from "./Autocomplete";
 export { default as Modal } from "./Modal";
 export { default as Navbar } from "./Navbar";
-export { default as Select } from "./Select";
 export { default as Toast } from "./Toast";
 export * from "./buttons";
 export * from "./inputs";
--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,2 +1,4 @@
 export { default as Input } from "./Input";
+export { default as Select } from "./Select";
 export { default as AnimatedSelect } from "./AnimatedSelect";
+export { default as Autocomplete } from "./Autocomplete";
