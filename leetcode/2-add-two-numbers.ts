c3ae0096062dc72f922ffb691110cd23697b631b
Hasan <dev.hahuaz@gmail.com>
2024-01-18 15:42:01 +0300

    chore: add leetcode 2 question

--- leetcode/2-add-two-numbers.ts
+++ leetcode/2-add-two-numbers.ts
@@ -1,11 +1,30 @@
 class ListNode {
+  // declare prop types
   val: number;
   next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
+
+  // initialize props
+  constructor(val: number, next?: ListNode | null) {
+    this.val = val;
     this.next = next === undefined ? null : next;
   }
 }
+
+/**
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
+ * example:
+ *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807
+ */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
   const headNode = new ListNode(0);
@@ -26,10 +45,18 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
   return headNode.next;
 }
 
-console.dir(
-  addTwoNumbers(
-    { val: 1, next: { val: 1, next: { val: 1, next: null } } },
-    { val: 1, next: { val: 1, next: { val: 9, next: null } } }
-  ),
-  { depth: 'unlimited' }
+const test1 = addTwoNumbers(
+  { val: 2, next: { val: 4, next: { val: 3, next: null } } },
+  { val: 5, next: { val: 6, next: { val: 4, next: null } } }
 );
+
+if (
+  test1?.val === 7 &&
+  test1?.next?.val === 0 &&
+  test1?.next?.next?.val === 8
+) {
+  console.log("test1 passed");
+  console.dir(test1, { depth: null });
+} else {
+  console.log("test1 failed");
+}
