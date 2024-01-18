8884c922a97e68544d7d49879a8eb7805fb72022
Hasan <dev.hahuaz@gmail.com>
2024-01-18 14:27:07 +0300

    chore: two sum question

--- leetcode/1-two-sum.ts
+++ leetcode/1-two-sum.ts
@@ -1,13 +1,43 @@
-function twoSum(nums: number[], target: number): number[] {
-  const hash: any = {};
+/**
+ * given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ *
+ * example:
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ */
+
+function twoSum(nums: number[], target: number): number[] | undefined {
+  // hash holds the number as key and index as value
+  const hash: { [key: number]: number } = {};
+
   for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-    if (complement in hash) {
-      return [hash[complement], i];
+    const num = nums[i];
+    const diff = target - num;
+
+    // if diff is seen in the loop, we have found the pair
+    if (hash[diff]) {
+      return [hash[diff], i];
     }
 
-    // save the index in hash for the number
-    hash[nums[i]] = i;
+    // store the num in hash to look for its pair in the next iteration
+    hash[num] = i;
   }
-  return hash;
+}
+
+const test1 = twoSum([2, 7, 11, 15], 9);
+if (test1?.[0] === 0 && test1?.[1] === 1) {
+  console.log("test1 passed");
+} else {
+  console.log("test1 failed");
+}
+
+const test2 = twoSum([3, 2, 4], 6);
+if (test2?.[0] === 1 && test2?.[1] === 2) {
+  console.log("test2 passed");
+} else {
+  console.log("test2 failed");
 }
