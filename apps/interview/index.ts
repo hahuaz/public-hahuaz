0e9224339bb45293a789f7586e65dba786a45ce6
Hasan <dev.hahuaz@gmail.com>
2025-03-07 03:26:54 +0300

    chore: configure ts to support top level await

--- apps/interview/index.ts
+++ apps/interview/index.ts
@@ -1,3 +1,9 @@
 const _myvar = "Hello World";
 
 console.log("Hello World");
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+await wait(5000);
+
+console.log("Hello World2");
