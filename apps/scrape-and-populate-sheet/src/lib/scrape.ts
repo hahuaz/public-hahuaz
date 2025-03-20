7b9f5482e0c8761046f9efb13be233e5aa0f33f0
Hasan <dev.hahuaz@gmail.com>
2025-03-20 09:24:55 +0300

    chore: puppeteer waits untill it gets a value

--- apps/scrape-and-populate-sheet/src/lib/scrape.ts
+++ apps/scrape-and-populate-sheet/src/lib/scrape.ts
@@ -6,11 +6,15 @@ export async function scrape({
   urlsToScrape,
   querySelector,
   USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+  isLocalTr = false,
 }: {
   urlsToScrape: string[];
   querySelector: string;
   USER_AGENT?: string;
+  isLocalTr?: boolean;
 }) {
+  const scrapeResult: ScrapeResult = [];
+
   const browser = await puppeteer.launch({
     headless: true,
   });
@@ -18,36 +22,50 @@ export async function scrape({
 
   await page.setUserAgent(USER_AGENT);
 
-  const scrapeResult: ScrapeResult = [];
-
   for (const url of urlsToScrape) {
     try {
       // console.log(`Scraping: ${url}`);
-      await page.goto(url);
-      await page.waitForSelector(querySelector);
+      await page.goto(url, { waitUntil: "networkidle2" });
+
+      // Wait until the element has a non-empty text value
+      await page.waitForFunction(
+        (selector) => {
+          const element = document.querySelector(selector);
+          return element?.textContent?.trim() !== "";
+        },
+        {}, // Options
+        querySelector
+      );
+
+      // await page.waitForSelector(querySelector);
 
       const scrapeValue = await page.evaluate((qs) => {
         const element = document.querySelector(qs);
         const textContent = element?.textContent?.trim();
         return textContent;
       }, querySelector);
+      // console.log(`scrapeValue: ${scrapeValue}`);
 
       // append latest scraped value to the file as a new line
       const resource = url.split("/").pop() as string;
+      // console.log(`resource: ${resource}`);
 
       if (scrapeValue === undefined) {
         console.error(`${url}: scrapeValue is undefined`);
         continue;
       }
 
-      // In some locales (e.g., tr_TR), a comma is used as the decimal separator and a dot as the thousands separator (e.g., 1.234,56). Normalize the value.
-      const formattedScrapeValue = scrapeValue
-        .replace(/\./g, "")
-        .replace(",", ".");
+      let normalizedScrapeValue = scrapeValue;
+      if (isLocalTr) {
+        // In some locales (e.g., tr_TR), a comma is used as the decimal separator and a dot as the thousands separator (e.g., 1.234,56). Normalize the value.
+        normalizedScrapeValue = normalizedScrapeValue
+          .replace(/\./g, "")
+          .replace(",", ".");
+      }
 
       scrapeResult.push({
         resource,
-        value: formattedScrapeValue,
+        value: normalizedScrapeValue,
       });
 
       // await wait(5000);
--- apps/scrape-and-populate-sheet/src/update-fund-prices.ts
+++ apps/scrape-and-populate-sheet/src/update-fund-prices.ts
@@ -1,37 +1,58 @@
 import { scrape, saveToSheet } from "@/lib/index.js";
 
-const SCRAPE_CONFIG_X = {
-  domain: "https://fintables.com/",
-  resources: [
-    "fonlar/ZBJ",
-    "fonlar/PPN",
-    "fonlar/BGP",
-    "fonlar/DBB",
-    "fonlar/GA1",
-    "fonlar/GYK",
-    "fonlar/GUB",
-    "fonlar/TI6",
-    "fonlar/MUT",
-    "fonlar/HMG",
-    "fonlar/OBI",
-    "fonlar/APT",
-    "fonlar/NRG",
-    "fonlar/FIT",
-  ],
-  querySelector:
-    "div.flex-shrink-0.relative span.inline-flex.items-center.tabular-nums",
-};
+import type { ScrapeResult } from "@/types/index.js";
 
-const urlsToScrape = SCRAPE_CONFIG_X.resources.map(
-  (resource) => `${SCRAPE_CONFIG_X.domain}${resource}`
-);
+const SITES_TO_SCRAPE = [
+  {
+    domain: "https://fintables.com/",
+    resources: [
+      "fonlar/ZBJ",
+      "fonlar/PPN",
+      "fonlar/BGP",
+      "fonlar/DBB",
+      "fonlar/GA1",
+      "fonlar/GYK",
+      "fonlar/GUB",
+      "fonlar/TI6",
+      "fonlar/MUT",
+      "fonlar/HMG",
+      "fonlar/OBI",
+      "fonlar/APT",
+      "fonlar/NRG",
+      "fonlar/FIT",
+      "fonlar/TP2",
+    ],
+    querySelector:
+      "div.flex-shrink-0.relative span.inline-flex.items-center.tabular-nums",
+    isLocalTr: true,
+  },
+  {
+    domain: "https://www.tradingview.com/",
+    resources: ["symbols/BIST-ALTIN"],
+    querySelector:
+      "#js-category-content > div.tv-react-category-header > div.js-symbol-page-header-root > div > div > div > div.quotesRow-pAUXADuj > div:nth-child(1) > div > div.lastContainer-JWoJqCpY > span.last-JWoJqCpY.js-symbol-last > span",
+  },
+];
 
 export async function updateFundPrices() {
-  const scrapeValues = await scrape({
-    urlsToScrape,
-    querySelector: SCRAPE_CONFIG_X.querySelector,
-  });
+  let allScrapeValues: ScrapeResult = [];
 
-  await saveToSheet(scrapeValues);
+  for (const site of SITES_TO_SCRAPE) {
+    const urlsToScrape = site.resources.map(
+      (resource) => `${site.domain}${resource}`
+    );
+    const scrapeValues = await scrape({
+      urlsToScrape,
+      querySelector: site.querySelector,
+      isLocalTr: site.isLocalTr,
+    });
+
+    allScrapeValues = allScrapeValues.concat(scrapeValues);
+  }
+
+  if (allScrapeValues.length > 0) {
+    await saveToSheet(allScrapeValues);
+  }
+
   console.log("updateFundPrices success.");
 }
