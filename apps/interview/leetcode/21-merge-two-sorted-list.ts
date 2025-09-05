1a354812d386d85ab14d490fd23bb629f09c1365
Hasan <dev.hahuaz@gmail.com>
2025-09-05 20:35:27 +0300

    feat: leet code 21

--- apps/interview/leetcode/21-merge-two-sorted-list.ts
+++ apps/interview/leetcode/21-merge-two-sorted-list.ts
@@ -1,3 +1,17 @@
+/**
+ * 21. Merge Two Sorted Lists
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted list. The new list should be made
+ * by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ * Example 1:
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ */
 class ListNode {
   val: number;
   next: ListNode | null;
@@ -7,30 +21,26 @@ class ListNode {
   }
 }
 
-function _mergeTwoLists(
+function mergeTwoLists(
   list1: ListNode | null,
   list2: ListNode | null
 ): ListNode | null {
-  const headNode = new ListNode(-1);
-  let prevNode = headNode;
+  const dummyHead = new ListNode(-1);
+  let curNode = dummyHead;
 
-  // while two list are not null
-  while (list1 && list2) {
-    if (list1.val < list2.val) {
-      prevNode.next = list1;
-
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      curNode.next = new ListNode(list1.val);
       list1 = list1.next;
-      prevNode = prevNode.next;
     } else {
-      prevNode.next = list2;
-
+      curNode.next = new ListNode(list2.val);
       list2 = list2.next;
-      prevNode = prevNode.next;
     }
+    curNode = curNode.next;
   }
 
-  // either one list is null or two list is null
-  prevNode.next = list1 || list2;
+  // if one of the list is not null, append it to the end
+  curNode.next = list1 !== null ? list1 : list2;
 
-  return headNode.next;
+  return dummyHead.next;
 }
