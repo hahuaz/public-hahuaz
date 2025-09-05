a3378c5c6eea6c203d6ce18762802ac7cb55fabf
Hasan <dev.hahuaz@gmail.com>
2025-09-05 21:29:22 +0300

    feat: leet code 121

--- apps/interview/leetcode/121-best-time-to-buy-and-sell-stock.ts
+++ apps/interview/leetcode/121-best-time-to-buy-and-sell-stock.ts
@@ -1,8 +1,23 @@
+/**
+ * 121. Best Time to Buy and Sell Stock
+ *
+ * You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+ *
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ *
+ * Return the maximum profit you can achieve from this transaction.
+ * If you cannot achieve any profit, return 0.
+ *
+ * Example 1:
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5.
+ *
+ */
+
+// O(n) time complexity
 function maxProfit(prices: number[]): number {
-  // Basically, we are trying to find the maximum difference between the numbers of the array
-
-  if (prices.length == 0) return 0;
-
   let maxProfit = 0;
   let buyPrice = 0;
 
@@ -10,14 +25,18 @@ function maxProfit(prices: number[]): number {
     // if it's first day, buy in order to calculate profit in the future
     if (i == 0) {
       buyPrice = prices[i];
+      continue;
     }
 
-    // if price got down, change buy point to see what proift we can make
+    // if price got increase, calc and save the profit for future compare
+    if (prices[i] >= buyPrice) {
+      maxProfit = Math.max(maxProfit, prices[i] - buyPrice);
+      continue;
+    }
+
+    // if price got down, change buy point to see what profit we can make
     if (prices[i] < buyPrice) {
       buyPrice = prices[i];
-    } else {
-      // if price got increase, calculate the profit
-      maxProfit = Math.max(maxProfit, prices[i] - buyPrice);
     }
   }
 
@@ -27,4 +46,16 @@ function maxProfit(prices: number[]): number {
   return maxProfit;
 }
 
-maxProfit([1, 30, 0, 3, 6, 4]);
+// O(n^2) time complexity
+function maxProfit2(prices: number[]): number {
+  let maxProfit = 0;
+
+  for (let buyDay = 0; buyDay < prices.length; buyDay++) {
+    for (let sellDay = buyDay + 1; sellDay < prices.length; sellDay++) {
+      const profit = prices[sellDay] - prices[buyDay];
+      maxProfit = Math.max(maxProfit, profit);
+    }
+  }
+
+  return maxProfit;
+}
